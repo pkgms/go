d0e30425cb965b0b01bd90ce4ec7241adde85a70
zc <zc2638@qq.com>
2020-10-28 14:58:24 +0800

    add logger hook

---
+++ logger/file.go
@@ -0,0 +1,88 @@
+/**
+ * Created by zc on 2020/10/28.
+ */
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	// DefaultPath 默认地址
+	DefaultPath = "_log"
+)
+
+type FileHook struct {
+	ws       map[logrus.Level]io.ReadWriteCloser
+	dir      string
+	datetime string
+}
+
+func NewFileHook(dir string) *FileHook {
+	if dir == "" {
+		dir = DefaultPath
+	}
+	return &FileHook{
+		dir: dir,
+		ws:  make(map[logrus.Level]io.ReadWriteCloser),
+	}
+}
+
+func (h *FileHook) SetDir(dir string) *FileHook {
+	filterDir := strings.TrimRight(dir, "/")
+	if filterDir != "" {
+		h.dir = filterDir
+	}
+	return h
+}
+
+func (h *FileHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+// 自定义钩子执行（默认协程安全）
+func (h *FileHook) Fire(e *logrus.Entry) error {
+	// 判断log文件是否变更
+	now := time.Now().Format("20060102")
+	path := h.dir + "/" + e.Level.String() + "_" + now + ".log"
+	if h.datetime != now {
+		h.datetime = now
+		if h.ws[e.Level] != nil {
+			if err := h.ws[e.Level].Close(); err != nil {
+				return err
+			}
+			h.ws[e.Level] = nil
+		}
+	}
+	if _, err := os.Stat(path); err == nil && h.ws[e.Level] != nil {
+		return nil
+	}
+
+	// 自动创建文件
+	var pathArr = strings.Split(path, "/")
+	var pathLen = len(pathArr)
+	if pathLen > 1 {
+		dir := strings.Join(pathArr[:pathLen-1], "/")
+		// 自动创建日志文件夹
+		_, err := os.Stat(dir)
+		if err != nil {
+			mkErr := os.MkdirAll(dir, os.ModePerm)
+			if mkErr != nil {
+				return mkErr
+			}
+		}
+	}
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		return err
+	}
+
+	h.ws[e.Level] = f
+	e.Logger.Out = h.ws[e.Level]
+	return nil
+}
---
+++ logger/frame.go
@@ -0,0 +1,70 @@
+/**
+ * Created by zc on 2020/10/28.
+ */
+package main
+
+import (
+	"bytes"
+	"github.com/sirupsen/logrus"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+type FrameContextHook struct {
+	ws  []string
+	cap uint32
+}
+
+func NewFrameContextHook(c uint32, ws []string) *FrameContextHook {
+	if ws == nil {
+		ws = []string{"github.com/sirupsen/logrus", "net/http", "/runtime"}
+	}
+	if c == 0 {
+		c = 30
+	}
+	return &FrameContextHook{ws: ws, cap: c}
+}
+
+func (f *FrameContextHook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+	}
+}
+
+func (f *FrameContextHook) Fire(entry *logrus.Entry) error {
+	pc := make([]uintptr, f.cap)
+	n := runtime.Callers(-1, pc)
+	if n > 0 {
+		pc = pc[:n]
+		frames := runtime.CallersFrames(pc)
+		var chains strings.Builder
+		for {
+			frame, more := frames.Next()
+			skip := false
+			for _, w := range f.ws {
+				if strings.Contains(frame.File, w) {
+					skip = true
+					break
+				}
+			}
+			if !skip {
+				var buffer bytes.Buffer
+				_, _ = buffer.WriteString(frame.File)
+				_, _ = buffer.WriteRune(':')
+				_, _ = buffer.WriteString(strconv.Itoa(frame.Line))
+				_, _ = buffer.WriteRune(' ')
+				_, _ = buffer.WriteString(frame.Function)
+				_, _ = buffer.WriteString("\n")
+				chains.Write(buffer.Bytes())
+			}
+			if !more {
+				break
+			}
+		}
+		entry.Data["chain"] = chains.String()
+	}
+	return nil
+}
